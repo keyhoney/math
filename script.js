dc7e8e036dfb939b1083e0f7c630670201ed239b
keyhoney <ip9mong@gmail.com>
2025-03-20 12:10:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 업뎃5
+// 업뎃6
 // Firebase 및 Firestore 관련 모듈 import
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
